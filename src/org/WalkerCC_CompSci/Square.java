1a3eb33ebbfe54a7b9d5e1c53d3ccec11bf8a1cb
maddobry000 <maddobry000@warren.k12.in.us>
2022-02-09 13:16:41 -0500

    square

--- src/org/WalkerCC_CompSci/Square.java
+++ src/org/WalkerCC_CompSci/Square.java
@@ -3,7 +3,7 @@ package org.WalkerCC_CompSci;
 public final class Square extends Rectangle{
 
     public Square(double sideLength) {
-        super();  //What goes here?
+        super(sideLength, sideLength);  //What goes here?
 
     }
 
@@ -12,6 +12,8 @@ public final class Square extends Rectangle{
     }
 
     public void setSideLength(double width){
+        this.length = width;
+        this.width = width;
         //What goes here?
     }
 
